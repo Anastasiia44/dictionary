1019ee7c087496e7ee854cdc8fa205de413b70ac
Anastasiia44 <106679067+Anastasiia44@users.noreply.github.com>
2022-07-18 23:58:59 +0300

    added footer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,26 @@ function App() {
       <div className="container">
         <h1>Dictionary</h1>
         <Dictionaty defaultKeyword="sunset" />
-        <footer className="App-footer"> Coded be Anastasiia Maliuha</footer>
+        <footer className="App-footer">
+          {" "}
+          This project was created by{" "}
+          <a
+            href="https://relaxed-rolypoly-3aaf9a.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Anastasiia Maliuha
+          </a>{" "}
+          and is
+          <a
+            href="https://github.com/Anastasiia44/dictionary"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
       </div>
     </div>
   );
