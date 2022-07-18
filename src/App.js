18a2ebd755aa7840af3b7020eedb83ffae7b19a3
Anastasiia44 <106679067+Anastasiia44@users.noreply.github.com>
2022-07-18 10:32:39 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>Hello</header>
+      <button type="button" className="btn btn-primary">
+        Primary
+      </button>
+    </div>
+  );
+}
+
+export default App;
